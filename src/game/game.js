5d05eaf4a0a341ed3aa195b8c5418be481b9e0fc
toddbrentlinger <toddbrentlinger@gmail.com>
2024-06-12 20:43:03 -0700

    Update Game class of game logic module
    Adds method to add PlayerHand given arguments of the Player
    instance and bet amount for the hand.
    Inside deal method, checks if any PlayerHands before dealing cards and
    changing game state.
    Updates references to array of PlayerHands to property in Player that
    holds the PlayerHands instead.
    Exports GameState enum class to be used in Game React Component
    and adds notes to describe each game state value.

--- src/game/game.js
+++ src/game/game.js
@@ -3,6 +3,21 @@ import Decks from './decks.js';
 import Player from './player.js';
 import Rules from './rules.js';
 
+/**
+ * Game States:
+ * 1) Preparing
+ * - Players place bets
+ * - Cards could be shuffled
+ * 2) Dealing
+ * - Cards are dealt to each Player and Dealer
+ * 3) PlayerPlaying
+ * - Each Player plays their hand(s)
+ * 4) DealerPlaying
+ * - Dealer plays their hand
+ * 5) Ending
+ * - Depending on outcome, give/take Player's money
+ */
+
 class GameState {
     static Preparing = new GameState('preparing');
     static Dealing = new GameState('dealing');
@@ -28,50 +43,71 @@ class Game {
     }
 
     setState(newState) {
-        if (!newState || !(newState instanceof GameState)) {
-            return;
-        }
+        if (!newState || !(newState instanceof GameState)) { return; }
 
         this.state = newState;
     }
 
     addPlayer(player) {
         // If playerName is NOT valid string, return
-        if (!player || !(player instanceof Player)) {
-            return;
-        }
+        if (!player || !(player instanceof Player)) { return; }
 
         this.players.push(player);
     }
 
+    addPlayerHand(player, bet) {
+        // If NOT valid Player, return without adding PlayerHand
+        if (!player || !(player instanceof Player)) { return; }
+
+        // If bet is below minimum, return without adding PlayerHand
+        if (bet < this.rules.bettingLimit.min) { return; }
+
+        // If bet is above maximum, clamp bet to maximum
+        if (bet > this.rules.bettingLimit.max) {
+            bet = this.rules.bettingLimit.max;
+        }
+
+        player.addHand(bet);
+    }
+
     deal() {
+        // If NO PlayerHands, do NOT deal
+        const playerHandsTotal = this.players.reduce((accum, player) => accum + player.hands.length, 0);
+
+        if (playerHandsTotal === 0) { return; }
+
         // If game state is NOT 'Preparing', do not deal
         if (this.state !== GameState.Preparing) { return; }
 
+        // Set new game state to 'Dealing'
         this.setState(GameState.Dealing);
 
         // Clear Dealer and Player's hands to make sure they're empty
         this.dealerHand.clear();
-        this.playerHands = [];
+        //this.playerHands = [];
         /*for (const player of this.players) {
           player.clearHands();
           player.addHand()
         }*/
 
         // Create initial PlayerHand for each Player
-        this.playerHands = this.players.map((player) => new PlayerHand(player, 20));
+        //this.playerHands = this.players.map((player) => new PlayerHand(player, 20));
 
         // Give first card to each initial PlayerHand, face up
-        for (const playerHand of this.playerHands) {
-            playerHand.addCard(this.decks.getTopCard());
+        for (const player of this.players) {
+            for (const playerHand of player.hands) {
+                playerHand.addCard(this.decks.getTopCard());
+            }
         }
 
         // Give first card to Dealer, face down
         this.dealerHand.addCard(this.decks.getTopCard());
 
         // Give second card to each initial PlayerHand, face up
-        for (const playerHand of this.playerHands) {
-            playerHand.addCard(this.decks.getTopCard());
+        for (const player of this.players) {
+            for (const playerHand of player.hands) {
+                playerHand.addCard(this.decks.getTopCard());
+            }
         }
 
         // Give second card to Dealer, face up
@@ -106,3 +142,4 @@ class Game {
 }
 
 export default Game;
+export { GameState, };
