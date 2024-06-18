e2c12c0aea312585f706a1e6557e582b16281a21
toddbrentlinger <toddbrentlinger@gmail.com>
2024-06-18 15:01:37 -0700

    Start Dealer component

---
+++ src/components/Dealer.jsx
@@ -0,0 +1,33 @@
+import Hand from './Hand';
+
+/** React component for single Dealer instance. */
+function Dealer({ cards }) {
+    /**
+     * Returns ReactElement of Dealer's hand of cards
+     * @returns {React.ReactElement}
+     */
+    const renderHand = function() {
+        // If NO cards, render nothing
+        if (cards.length === 0) { return; }
+        
+        return (
+            <Hand
+                handObj={{
+                    hand: null,
+                    bet: 0,
+                    hasStand: false,
+                    cards: cards,
+                }}
+            />
+        );
+    };
+
+    return (
+        <section id="dealer" className="player">
+            <span className="player-info">Dealer</span>
+            {renderHand()}
+        </section>
+    );
+}
+
+export default Dealer;
