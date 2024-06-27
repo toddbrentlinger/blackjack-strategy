357243b4893f765c16985467be7df58de07f8ad1
toddbrentlinger <toddbrentlinger@gmail.com>
2024-06-26 17:03:06 -0700

    Add file to hold JavaScript variables used in multiple other scripts

---
+++ src/components/constants.js
@@ -0,0 +1,2 @@
+export const RANKS = ['ace', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'jack', 'queen', 'king'];
+export const SUITS = ['spade', 'heart', 'diamond', 'club'];
