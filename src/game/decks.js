45f8277bc3dc63a451128bdbc32bd2085ef4ac1f
toddbrentlinger <toddbrentlinger@gmail.com>
2024-09-15 11:40:54 -0700

    Update Decks class to be more memory and time efficient
    Replaces two different arrays to hold cards used and unused with single array
    to hold all cards and variable for moving index of current top card. This avoids
    arrays allocating or deallocating memory frequently.
    Using two different arrays to hold cards used and not used changes the length
    of both arrays every time a card is taken from the deck. This can be slow since
    memory is deallocated and allocated frequently in the arrays.
    Create method that returns the fractional number betwen [0,1] representing the
    number of cards left in the decks. This is a public method called outside the
    Decks class to determine when to reshuffle.
    Adds misc comments and docstrings.

--- src/game/decks.js
+++ src/game/decks.js
@@ -1,62 +1,89 @@
 import Card, { Suit, Rank, } from './card.js';
 
 class Decks {
+    /**
+     * @constructor
+     * @param {number} nDecks - Number of decks, each 52 cards, to use
+     */
     constructor(nDecks = 1) {
         // Throw error if nDecks is less than 0
         if (nDecks < 1) {
             throw new Error('Number of decks needs to be more 1 or more.');
         }
 
-        this.nDecks = nDecks;
-        this.cardsNotUsed = []; // Array used as stack to hold Cards not yet used
-        this.cardsUsed = []; // Array used as stack to hold Cards already used
+        this.nDecks = nDecks; // Total number of decks to use
+        this.cards = []; // Array to hold all cards for all decks
+        this.topCardIndex = -1; // Moving index in cards array of current top card
 
         this.populate();
         this.shuffle();
     }
 
-    getTopCard() {
-        // If no more cards can be used, shuffle
-        if (!this.cardsNotUsed.length) {
-            return;
+    /**
+     * Returns fractional number between [0,1] representing the number of cards 
+     * left in the decks.
+     * @returns {number}
+     */
+    getFractionThroughDecks() {
+        if (this.topCardIndex === -1) {
+            return 0;
         }
 
-        const topCard = this.cardsNotUsed.pop();
-        this.cardsUsed.push(topCard);
+        return ((this.topCardIndex + 1) / this.cards.length);
+    }
+
+    /**
+     * Returns top card of all decks.
+     * @returns {Card}
+     */
+    getTopCard() {
+        // If no more cards, throw error
+        if (this.topCardIndex === -1) {
+            throw new Error('No more cards in deck');
+        }
+
+        // Get reference to current top card
+        const topCard = this.cards[this.topCardIndex];
+        
+        // Decrement index of top card to point to new card (or -1 if last card)
+        this.topCardIndex--;
+        
         return topCard;
     }
 
+    /** Fills decks with cards. */
     populate() {
-        // Empty cardsNotUsed array in case it has any cards
-        this.cardsNotUsed = [];
+        // Clear any existing cards
+        this.cards = [];
 
-        // Add nDecks of cards to cardsNotUsed array
+        // Add nDecks of cards to cards array
         for (let n = 0; n < this.nDecks; n++) {
             Object.keys(Suit).forEach((suit) => {
                 Object.keys(Rank).forEach((rank) => {
-                    this.cardsNotUsed.push(new Card(Suit[suit], Rank[rank]));
+                    this.cards.push(new Card(Suit[suit], Rank[rank]));
                 });
             });
         }
+
+        // Set topCardIndex to point to last card in card array (top card)
+        this.topCardIndex = this.cards.length - 1;
     }
 
+    /** Randomly shuffles positions of cards in the decks. */
     shuffle() {
         let iRand;
 
-        // Add all cardsUsed items to cardsNotUsed array so all cards back in same array
-        this.cardsNotUsed = this.cardsNotUsed.concat(this.cardsUsed);
-
-        // Reset cardsUsed array to empty array
-        this.cardsUsed = [];
-
-        // Randomize order of cardsNotUsed array
-        for (let i = 0; i < this.cardsNotUsed.length; i++) {
+        // Randomize order of cards array
+        for (let i = 0; i < this.cards.length; i++) {
             // Find random index in array
-            iRand = Math.floor(Math.random() * this.cardsNotUsed.length);
+            iRand = Math.floor(Math.random() * this.cards.length);
 
             // Switch cards in i and iRand indices
-            [this.cardsNotUsed[i], this.cardsNotUsed[iRand]] = [this.cardsNotUsed[iRand], this.cardsNotUsed[i]];
+            [this.cards[i], this.cards[iRand]] = [this.cards[iRand], this.cards[i]];
         }
+
+        // Set topCardIndex to point to last card in card array (top card)
+        this.topCardIndex = this.cards.length - 1;
     }
 }
 
