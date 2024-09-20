64c22679449b5e3fc6841f940e09de6f73c8e237
toddbrentlinger <toddbrentlinger@gmail.com>
2024-09-19 18:29:53 -0700

    Update Game class in game.js
    Adds methods to start Player play and Dealer play game states.
    Adds methods to start and end a single game round.
    Adds method to increment the index of current Player in a single game round.
    Adds method to check if a Player object passed as an argument is current active
    Player in the game round.
    Adds methods that take specific Player object passed as an argument to hit,
    stand, double down, split, and start method to surrender.
    Adds method that returns suggested strategy of a specific Player object's active PlayerHand.
    Removes imports no longer needed.
    Changes default current player index to -1 for NO players instead of zero.
    Adds getter to return current active Player for the game round OR returns null
    for no Player turn.
    Adds misc comments and docstrings.

--- src/game/game.js
+++ src/game/game.js
@@ -1,7 +1,8 @@
-import Hand, { DealerHand, PlayerHand, } from './hand.js';
+import { DealerHand, } from './hand.js';
 import Decks from './decks.js';
 import Player from './player.js';
 import Rules from './rules.js';
+import blackjackStrategies from './strategy.js';
 
 /**
  * Game States:
@@ -17,7 +18,6 @@ import Rules from './rules.js';
  * 5) Ending
  * - Depending on outcome, give/take Player's money
  */
-
 class GameState {
     static Preparing = new GameState('preparing');
     static Dealing = new GameState('dealing');
@@ -25,12 +25,21 @@ class GameState {
     static DealerPlaying = new GameState('dealerPlaying');
     static Ending = new GameState('ending');
 
+    /**
+     * @constructor
+     * @param {string} name 
+     */
     constructor(name) {
         this.name = name;
     }
 }
 
+/** Single game of Blackjack. */
 class Game {
+    /**
+     * @constructor
+     * @param {number} nDecks - Number of 52-card decks to use in the game
+     */
     constructor(nDecks = 6) {
         this.state = GameState.Preparing;
         this.players = [];
@@ -39,15 +48,33 @@ class Game {
         this.decks = new Decks(nDecks);
         this.rules = new Rules();
         this.shuffleUsesCSM = false; // CSM: Continuous-shuffling-machine
-        this.currentPlayerIndex = 0; // Used to keep correct play order of Players
+        this.currentPlayerIndex = -1; // Used to keep correct play order of Players
     }
 
+    // Getters/Setters
+
+    /** Returns current Player object, else null if NO Player has current turn. */
+    get currentPlayer() {
+        return (this.currentPlayerIndex === -1) 
+            ? null
+            : this.players[this.currentPlayerIndex];
+    }
+
+    /**
+     * Sets new GameState of the Blackjack game.
+     * @param {GameState} newState - New GameState to change the current game
+     */
     setState(newState) {
+        // Check tht newState argument is valid
         if (!newState || !(newState instanceof GameState)) { return; }
 
         this.state = newState;
     }
 
+    /**
+     * Adds new Player to the Blackjack game.
+     * @param {Player} player - Player object to add to Blackjack game
+     */
     addPlayer(player) {
         // If playerName is NOT valid string, return
         if (!player || !(player instanceof Player)) { return; }
@@ -55,6 +82,11 @@ class Game {
         this.players.push(player);
     }
 
+    /**
+     * Adds new PlayerHand to a specific Player with a specific bet amount.
+     * @param {Player} player 
+     * @param {number} bet 
+     */
     addPlayerHand(player, bet) {
         // If NOT valid Player, return without adding PlayerHand
         if (!player || !(player instanceof Player)) { return; }
@@ -70,10 +102,10 @@ class Game {
         player.addHand(bet);
     }
 
+    /** Deals cards to each Player and Dealer. */
     deal() {
         // If NO PlayerHands, do NOT deal
         const playerHandsTotal = this.players.reduce((accum, player) => accum + player.hands.length, 0);
-
         if (playerHandsTotal === 0) { return; }
 
         // If game state is NOT 'Preparing', do not deal
@@ -85,10 +117,10 @@ class Game {
         // Clear Dealer and Player's hands to make sure they're empty
         this.dealerHand.clear();
         //this.playerHands = [];
-        /*for (const player of this.players) {
-          player.clearHands();
-          player.addHand()
-        }*/
+        // for (const player of this.players) {
+        //   player.clearHands();
+        //   player.addHand()
+        // }
 
         // Create initial PlayerHand for each Player
         //this.playerHands = this.players.map((player) => new PlayerHand(player, 20));
@@ -112,32 +144,364 @@ class Game {
 
         // Give second card to Dealer, face up
         this.dealerHand.addCard(this.decks.getTopCard());
+
+        // Set Player index so 0-index Player plays first
+        this.currentPlayerIndex = 0;
     }
 
-    hit(hand) {
-        if (!hand || !(hand instanceof Hand)) {
+    /** Starts game state where Players play their hands. */
+    startPlayerPlay() {
+        // Find first Player with Hand NOT initially dealt blackjack
+        let playerIndex = 0;
+        while ((playerIndex < this.players.length) 
+            && (this.players[playerIndex].hands[0].hasBlackjack())
+        ) {
+            playerIndex++;
+        }
+
+        // If playerIndex outside range of players array, all Players have blackjack
+        if (playerIndex === this.players.length) {
+            this.startDealerPlay();
+            return;
+        }
+
+        // Reset currentPlayerIndex to start with first valid Player
+        this.currentPlayerIndex = playerIndex;
+
+        // Set new game state to 'PlayerPlaying'
+        this.setState(GameState.PlayerPlaying);
+    }
+
+    /** Starts game state where Dealer plays their hand. */
+    startDealerPlay() {
+        // Set DealerPlaying game state
+        this.setState(GameState.DealerPlaying);
+
+        // Turn Dealer second card face up
+        this.dealerHand.turnSecondCardFaceUp();
+
+        // Get initial value of Dealer's two cards
+        let dealerTotal = this.dealerHand.getTotal();
+        
+        // Keep hitting OR stand depending on current value of Dealer's cards
+        while (dealerTotal <= 17) {
+            // If Hand is soft (at least one Ace)
+            if (this.dealerHand.isSoft()) {
+                if (dealerTotal < 17) {
+                    // Hit
+                    this.dealerHand.addCard(this.decks.getTopCard());
+                }
+                // Else dealerTotal === 17 
+                else {
+                    if (!this.rules.dealerStandsOnSoft17) {
+                        // Hit
+                        this.dealerHand.addCard(this.decks.getTopCard());
+                    }
+                    break;
+                }
+            }
+            // Else Hand is NOT soft 
+            else if (dealerTotal < 17) {
+                // Hit
+                this.dealerHand.addCard(this.decks.getTopCard());
+            }
+
+            // Increase value of Dealer's cards after new card added for next loop
+            dealerTotal = this.dealerHand.getTotal();
+        }
+    }
+
+    /** Starts new round of the Blackjack game. */
+    startRound() {
+        this.setState(GameState.Preparing);
+    }
+
+    /** Ends current round of the Blackjack game. */
+    endRound() {
+        // Set Ending game state
+        this.setState(GameState.Ending);
+
+        // Award any winnings and take any bets from each Player
+        // Clear cards from hands
+        for (const player of this.players) {
+            player.handleEndRound(this.dealerHand, this.rules.blackjackPayout);
+        }
+
+        // Check if need to shuffle (50% through deck)
+        if (this.decks.getFractionThroughDecks() < .5) {
+            this.decks.shuffle();
+        }
+    }
+
+    /** Increments to next Player turn. */
+    incrementPlayerIndex() {
+        // If still Players after current Player, increment index value
+        if (this.currentPlayerIndex < (this.players.length - 1)) {
+            this.currentPlayerIndex++;
+        }
+        // Else NO more players, reset index value and start Dealer play 
+        else {
+            this.currentPlayerIndex = -1;
+            this.startDealerPlay();
+        }
+    }
+
+    /**
+     * Returns true if it's a specific Player's turn, else returns false.
+     * @param {Player} player - Player to check if it's their turn to play 
+     * @returns {boolean}
+     */
+    isPlayerTurn(player) {
+        // If NOT valid Player, return false
+        if (!player || !(player instanceof Player)) {
+            return false;
+        }
+
+        // If NOT Player's turn, return false
+        if (this.players[this.currentPlayerIndex] !== player) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Hits current active hand of a specific Player.
+     * @param {Player} player - Player to hit on their current active PlayerHand
+     */
+    hit(player) {
+        // If wrong game state, return
+        if (this.state !== GameState.PlayerPlaying) {
+            return;
+        }
+
+        // If NOT Player turn, return
+        if (!this.isPlayerTurn(player)) {
+            return;
+        }
+
+        // If NO PlayerHand, increment to next Player and return
+        if (player.currentHand === null) {
+            this.incrementPlayerIndex();
             return;
         }
 
         // If Hand is already bust, do not add new card
-        if (hand.isBust()) {
+        // if (playerHand.isBust()) {
+        //     return;
+        // }
+
+        // Add deck top card to Hand
+        player.hit(this.decks.getTopCard());
+
+        // TODO: Player could now be finished with this hand and could 
+        // either start a new hand after splitting OR next player could start.
+        if (player.currentHand === null) {
+            this.incrementPlayerIndex();
+        }
+    }
+
+    /**
+     * Stands current active hand of a specific Player.
+     * @param {Player} player - Player to stand on their current active PlayerHand
+     */
+    stand(player) {
+        // TODO: START - Separate into separate function
+        // If wrong game state, return
+        if (this.state !== GameState.PlayerPlaying) {
+            return;
+        }
+
+        // If NOT Player turn, return
+        if (!this.isPlayerTurn(player)) {
+            return;
+        }
+
+        // TODO: END
+        
+        player.stand();
+
+        // If Player has no more hands, increment to next Player
+        if (player.currentHand === null) {
+            this.incrementPlayerIndex();
+        }
+    }
+
+    /**
+     * Doubles down current active hand of a specific Player.
+     * @param {Player} player - Player to double down on their current active PlayerHand
+     */
+    doubleDown(player) {
+        // If wrong game state, return
+        if (this.state !== GameState.PlayerPlaying) {
+            return;
+        }
+
+        // If NOT Player turn, return
+        if (!this.isPlayerTurn(player)) {
+            return;
+        }
+
+        // If NO PlayerHand, increment to next Player and return
+        if (player.currentHand === null) {
+            this.incrementPlayerIndex();
             return;
         }
 
         // Add deck top card to Hand
-        hand.addCard(this.decks.getTopCard());
+        player.doubleDown(this.decks);
+
+        // Player could either move to a next hand after splitting OR next player could start.
+        if (player.currentHand === null) {
+            this.incrementPlayerIndex();
+        }
     }
 
-    stand(playerHand) {
-        if (!playerHand || !(playerHand instanceof PlayerHand)) {
+    /**
+     * Splits current active hand of a specific Player.
+     * @param {Player} player - Player to split on their current active PlayerHand
+     */
+    split(player) {
+        // If wrong game state, return
+        if (this.state !== GameState.PlayerPlaying) {
             return;
         }
 
-        playerHand.stand();
+        // If NOT Player turn, return
+        if (!this.isPlayerTurn(player)) {
+            return;
+        }
+
+        // Check if splitting is allowed in rules
+        if (!this.rules.split.isAllowed) {
+            return;
+        }
+
+        // Check if can add another hand using rules
+        if (player.currentHand.cards.length === this.rules.split.maxHands) {
+            return;
+        }
+
+        // Check if Player can split
+        if (!player.canSplit()) { 
+            return; 
+        }
+
+        player.split(this.decks);
     }
 
-    split(player, playerHandIndex) {
-        player.playerHands[playerHandIndex];
+    /**
+     * Surrenders current active hand of a specific Player.
+     * @param {Player} player - Player to surrender on their current active PlayerHand
+     */
+    surrender(player) {
+        // If wrong game state, return
+        if (this.state !== GameState.PlayerPlaying) {
+            return;
+        }
+
+        // If NOT Player turn, return
+        if (!this.isPlayerTurn(player)) {
+            return;
+        }
+
+        // TODO...
+    }
+
+    /**
+     * Returns object with data about the suggested strategy of a specific 
+     * Player's active hand.
+     * @param {Player} player - Player to hit on their current active PlayerHand
+     * @returns {Object} obj
+     * @returns {string} obj.action - Description of the suggest action
+     * @returns {string} obj.type - String representation of the type of strategy grid (Base, Ace, Split)
+     * @returns {number} obj.row - Row of the strategy grid
+     * @returns {number} obj.col - Column of the strategy grid
+     */
+    getBasicStrategyAction(player) {
+        // If wrong game state, return
+        if (this.state !== GameState.PlayerPlaying) {
+            return;
+        }
+
+        // If NOT Player turn, return
+        if (!this.isPlayerTurn(player)) {
+            return;
+        }
+
+        // Get current active PlayerHand
+        const playerHand = player.currentHand;
+
+        // If no active PlayerHand, return
+        if (playerHand === null) {
+            return;
+        }
+        
+        /** Column of strategy grid based on Dealer's hand */
+        const col = this.dealerHand.getTotal() - 2;
+
+        /** Row of strategy grid */
+        let row;
+        /** String representation of the type of strategy grid (Base, Ace, Split) */
+        let strategyType;
+        /** BlackjackStrategyGrid object that matches Player hand */
+        let strategyGrid;
+        
+        // Check for splitting strategy
+        if (playerHand.canSplit() 
+            && this.rules.split.isAllowed
+            && (player.hands.length < this.rules.split.maxHands)
+        ) {
+            /**
+             * Since both cards have same value, convert value of first card 
+             * to index of strategy.
+             */
+            row = (playerHand.cards[0].rank.value === 1) 
+                ? 9 
+                : (playerHand.cards[0].rank.value - 2);
+
+            strategyGrid = blackjackStrategies.basic.split;
+            strategyType = 'Split';
+        }
+
+        // Check for soft hands (at least one Ace)
+        else if (playerHand.isSoft() && (playerHand.getTotal() < 20)) {
+            // Find value of non-Ace in Player hand
+            row = playerHand.cards[0].rank.value;
+            if (row === 1) {
+                row = playerHand.cards[1].rank.value;
+            }
+            if (row === 1) {
+                row = 9;
+            } else {
+                row -= 2;
+            }
+
+            strategyGrid = blackjackStrategies.basic.ace;
+            strategyType = 'Ace';
+        }
+
+        // Check for basic strategy
+        else {
+            row = playerHand.getTotal() - 3;
+
+            strategyGrid = blackjackStrategies.basic.base;
+            strategyType = 'Base';
+        }
+
+        console.log(
+            `Suggested Strat (${strategyType}):`,
+            `row: ${row}`,
+            `col: ${col}`, 
+            strategyGrid[row][col].desc
+        );
+
+        return {
+            action: strategyGrid[row][col].desc,
+            type: strategyType,
+            row,
+            col,
+        };
     }
 }
 
