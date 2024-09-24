7cf6b980d9eae4c7acc7891067ddcde5a701c69d
toddbrentlinger <toddbrentlinger@gmail.com>
2024-09-23 18:58:43 -0700

    Update Hand React component
    Adds boolean property canPlayerFlip to allow User to flip Player cards but NOT
    Dealer cards.
    Adds element to display the value of the bet of the current hand.
    Moves both element to hold cards AND bet value element into container element.

--- src/components/Hand.jsx
+++ src/components/Hand.jsx
@@ -1,19 +1,31 @@
 import Card from './Card';
 import './Hand.scss';
 
-function Hand({ handObj }) {
+function Hand({ handObj, canPlayerFlip = true}) {
     return (
-        <div className="hand">
+        <div className="hand-container">
+            <div className="hand">
+                {
+                    handObj.cards.map((cardObj) => (
+                        <Card 
+                            rank={cardObj.card.rank.name} 
+                            suit={cardObj.card.suit.name} 
+                            key={cardObj.card.id} 
+                            initialIsFaceUp={cardObj.initialIsFaceUp}
+                            dealDelay={cardObj.dealDelay}
+                            canPlayerFlip={canPlayerFlip}
+                        />
+                    ))
+                }
+            </div>
             {
-                handObj.cards.map((cardObj) => (
-                    <Card 
-                        rank={cardObj.card.rank.name} 
-                        suit={cardObj.card.suit.name} 
-                        key={cardObj.card.id} 
-                        initialIsFaceUp={cardObj.initialIsFaceUp}
-                        dealDelay={cardObj.dealDelay}
-                    />
-                ))
+                (handObj.hand === null)
+                    ? null 
+                    : (
+                        <div>
+                            <div>Bet: {handObj.bet}</div>
+                        </div>
+                    )
             }
         </div>
     );
