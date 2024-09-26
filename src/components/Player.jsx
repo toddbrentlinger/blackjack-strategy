e13e1286266319edea7dbbf16d643a6e21adc96e
toddbrentlinger <toddbrentlinger@gmail.com>
2024-09-26 16:15:05 -0700

    Update Player React component
    Adds React properties for handler function for Player choosing to hit, stand,
    double down, split, surrender, and display suggested strategy, as well as
    minimum bet amount allowed in the game.
    Adds handler functions in Player React component that uses the handler functions
    passed as React props and passing the Player object as an argument.
    Adds useEffect to reset the bet amount state when a new round is started.
    Adds 'min' attribute to input element of bet form using min bet prop.
    Adds method to render all Player options using handler functions previously
    described.
    Adds method to render all Player hands.
    Adds render cases for game states 'DealerPlaying' and 'Ending'.
    Adds misc comments and docstrings.

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { GameState } from "../game/game";
 import Hand from "./Hand";
 import "./Player.scss";
 
+/** Time to delay Player making another action. */
+//const PLAYER_ACTION_DELAY = 1000;
+
 /** React component for single Player instance. */
-function Player({ playerObj, gameState, handleBet }) {
+function Player({ playerObj, gameState, minBet, handleBet, handleHit, handleStand, handleDoubleDown, handleSplit, handleSurrender, handleStrategy, }) {
     /** Amount of bet for current round of blackjack. */
     const [bet, setBet] = useState(null);
+
+    /** Reference to previous bet amount to populate bet form in next round. */
+    //const previousBet = useRef(minBet);
+
+    /** Reference to timeout used for Player component. */
+    //const timeoutId = useRef(null);
+
+    // Hooks - useEffect
+
+    // Resets bet amount to null when game state changes to 'Preparing'
+    useEffect(() => {
+        if ((gameState === GameState.Preparing)) {
+            setBet(null);
+        }
+    }, [gameState]);
+
+    // Methods
 
     /**
      * Event handler for Player bet form submit event.
@@ -25,6 +45,66 @@ function Player({ playerObj, gameState, handleBet }) {
     };
 
     /**
+     * Event handler for Player choosing to hit.
+     * @param {Event} e 
+     */
+    const handleHitClick = function(e) {
+        e.preventDefault();
+
+        handleHit(playerObj.player);
+    };
+
+    /**
+     * Event handler for Player choosing to stand.
+     * @param {Event} e 
+     */
+    const handleStandClick = function(e) {
+        e.preventDefault();
+
+        handleStand(playerObj.player);
+    };
+
+    /**
+     * Event handler for Player choosing to double down.
+     * @param {Event} e 
+     */
+    const handleDoubleDownClick = function(e) {
+        e.preventDefault();
+        
+        handleDoubleDown(playerObj.player);
+    }
+
+    /**
+     * Event handler for Player choosing to split.
+     * @param {Event} e 
+     */
+    const handleSplitClick = function(e) {
+        e.preventDefault();
+
+        handleSplit(playerObj.player);
+    };
+
+    /**
+     * Event handler for Player choosing to surrender.
+     * @param {Event} e 
+     */
+    const handleSurrenderClick = function(e) {
+        e.preventDefault();
+
+        handleSurrender(playerObj.player);
+    };
+
+    /**
+     * Event handler for Player choosing to get suggested strategy.
+     * @param {Event} e 
+     */
+    const handleStrategyClick = function(e) {
+        e.preventDefault();
+
+        handleStrategy(playerObj.player);
+    };
+
+    /**
      * Returns ReactElement of form to place bet for next Hand.
      * @returns {React.ReactElement}
      */
@@ -33,7 +113,12 @@ function Player({ playerObj, gameState, handleBet }) {
             <form onSubmit={handleBetFormSubmit} action="" method="post">
                 <label>
                     Bet:
-                    <input type="number" name="bet" autoFocus={true} />
+                    <input 
+                        type="number" 
+                        name="bet" 
+                        min={minBet}
+                        required
+                        autoFocus={true} />
                 </label>
                 <button>Place Bet</button>
             </form>
@@ -41,7 +126,7 @@ function Player({ playerObj, gameState, handleBet }) {
     };
 
     /**
-     * Returns ReactElement that displays info about the Player
+     * Returns ReactElement that displays info about the Player.
      * @returns {React.ReactElement}
      */
     const renderPlayerInfo = function() {
@@ -54,8 +139,51 @@ function Player({ playerObj, gameState, handleBet }) {
     };
 
     /**
-     * Returns ReactElement of Player.
+     * Returns ReactElement that displays Player action options.
      * @returns {React.ReactElement}
+     */
+    const renderHandOptions = function() {
+        const currentHand = playerObj.player.currentHand;
+
+        return (
+            <div className="player-options">
+                <button onClick={handleHitClick}>Hit</button>
+                <button onClick={handleStandClick}>Stand</button>
+                {
+                    (currentHand.cards.length === 2)
+                        ? (<button onClick={handleDoubleDownClick}>Double Down</button>)
+                        : null
+                }
+                {
+                    playerObj.player.canSplit()
+                     ? (<button onClick={handleSplitClick}>Split</button>)
+                     : null
+                }
+                {
+                    (currentHand.cards.length < 3)
+                        ? (<button onClick={handleSurrenderClick}>Surrender</button>)
+                        : null
+                }
+                <button onClick={handleStrategyClick}>Suggested Action</button>
+            </div>
+        );
+    };
+
+    /**
+     * Returns ReactElement that displays all hands of the Player.
+     * @returns {React.ReactElement|null}
+     */
+    const renderHands = function() {
+        if (playerObj.hands.length === 0) { return null; }
+        
+        return playerObj.hands.map((handObj) => (
+            <Hand key={handObj.hand.id} handObj={handObj} />
+        ));
+    };
+
+    /**
+     * Returns ReactElement of Player.
+     * @returns {React.ReactElement|undefined}
      */
     const renderPlayer = function() {
         switch(gameState) {
@@ -64,19 +192,28 @@ function Player({ playerObj, gameState, handleBet }) {
                     <section className="player">
                         {renderPlayerInfo()}
                         {(bet === null) ? renderBetForm() : <div>Bet: <span>{bet}</span></div>}
+                        <div className="hand"></div>
                     </section>
                 );
             case GameState.Dealing:
-            case GameState.PlayerPlaying:
             case GameState.DealerPlaying:
+            case GameState.Ending:
                 return (
                     <section className="player">
                         {renderPlayerInfo()}
-                        {(playerObj.hands.length === 0) ? null : <Hand handObj={playerObj.hands[0]} />}
+                        {renderHands()}
+                        {/* {(playerObj.hands.length === 0) ? null : <Hand handObj={playerObj.hands[0]} />} */}
                     </section>
                 );
-            case GameState.Ending:
-                return;
+            case GameState.PlayerPlaying:
+                return (
+                    <section className="player">
+                        {renderPlayerInfo()}
+                        {renderHands()}
+                        {/* {(playerObj.hands.length === 0) ? null : <Hand handObj={playerObj.hands[0]} />} */}
+                        {playerObj.isPlayerTurn ? renderHandOptions() : null}
+                    </section>
+                );
             default:
                 return;
         }
