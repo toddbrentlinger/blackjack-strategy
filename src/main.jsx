9ba2e53088259b1f69430dffac263a63a8ac7303
toddbrentlinger <toddbrentlinger@gmail.com>
2024-10-02 19:38:33 -0700

    Update main.jsx file
    Removes unused imports.
    Removes code used to test Game instance using just console commands.

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-import Player from './game/player.js'
-import Game from './game/game.js';
-
-let game = new Game();
-window.game = game;
-game.addPlayer(new Player('Todd', 1000));
-game.addPlayer(new Player('Indiana Jones', 1000));
-game.addPlayer(new Player('James Bond', 10000));
-game.deal();
-console.log(game.dealerHand.toString());
-console.log(game.playerHands.map(hand => hand.toString()));
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
